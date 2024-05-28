efc13779cc4ec8f44edc4b77b48a0fa6aeab8f17
Asep Bagja Priandana <bepitulaz@gmail.com>
2024-05-28 16:51:14 +0300

    Upgrade render and redwood (#17)

---
+++ sonastik-ee/api/db/migrations/20240408204251_add_user/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "hashedPassword" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "resetToken" TEXT,
+    "resetTokenExpiresAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
