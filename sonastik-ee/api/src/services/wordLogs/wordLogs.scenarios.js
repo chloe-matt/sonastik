bf33b1993d4cee1d752680c403f2951ac5f1f366
Asep Bagja Priandana <bepitulaz@gmail.com>
2025-04-09 15:51:47 +0300

    Delete redwood code

--- sonastik-ee/api/src/services/wordLogs/wordLogs.scenarios.js
+++
@@ -1,6 +0,0 @@
-export const standard = defineScenario({
-  wordLog: {
-    one: { data: { word: 'String', updatedAt: '2024-02-26T18:01:06.399Z' } },
-    two: { data: { word: 'String', updatedAt: '2024-02-26T18:01:06.399Z' } },
-  },
-})
